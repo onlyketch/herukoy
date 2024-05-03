14c96af80b671c14c14e0806bc210b4d9b637eb5
Andrey <byketch@yandex.ru>
2024-05-03 12:24:11 +0300

    добавление интерактива

--- js/script.js
+++ js/script.js
@@ -14,5 +14,30 @@ document.addEventListener('DOMContentLoaded', function() {
     logo.addEventListener('mouseout', function() {
       logo.setAttribute('class', 'header__logo-img');
     });
+
+    //menu
+    let gallery = document.querySelector('.gallery');
+    let about = document.querySelector('.about');
+    let contact = document.querySelector('.contact');
+
+    let aboutLink = document.querySelector('#about');
+    let galleryLink = document.querySelector('#gallery');
+    let contactLink = document.querySelector('#contact');
+
+    const links = [galleryLink, aboutLink, contactLink];
+    const sections = [gallery, about, contact];
+
+    for (let j = 0; j < links.length; j++) {
+      links[j].addEventListener('click', function(e) {
+        e.preventDefault();
+        for (let i = 0; i < sections.length; i++) {
+          sections[i].classList.add('invisible');
+        }
+  
+        sections[j].classList.remove('invisible');
+      });
+    }
+
+    
     
 });
