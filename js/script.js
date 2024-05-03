aa857659a92f1299562dfc8360a177628d06edb8
Andrey <byketch@yandex.ru>
2024-05-03 07:36:32 +0300

    анимация лого, ховеры

--- js/script.js
+++ js/script.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     Fancybox.bind("[data-fancybox]", {
         // Your custom options
       });
+
+    let logo = document.querySelector('.header__logo-img');
+    
+    logo.addEventListener('mouseover', function() {
+      let className = logo.getAttribute('class');
+      let newClassName = className + ' animate__animated animate__rubberBand';
+      logo.setAttribute('class', newClassName);
+    });
+
+    logo.addEventListener('mouseout', function() {
+      logo.setAttribute('class', 'header__logo-img');
+    });
+    
 });
